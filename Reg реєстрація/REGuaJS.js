e7c015d7e8a6563a7ba012f74f4a263e60ddd5e0
ToNaza <misterket37@gmail.com>
2024-03-26 20:35:00 +0200

    test 3

--- Reg реєстрація/REGuaJS.js
+++ Reg реєстрація/REGuaJS.js
@@ -29,3 +29,14 @@ function submitForm() {
     // Запобігаємо відправку форми за замовчуванням
     return false;
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz29ADCinQEKGjWdEGcy2aqtJQk2sEMHUgm1jUv_fGOVzoWI8W3BrylngwiHgwi2Ce5/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
