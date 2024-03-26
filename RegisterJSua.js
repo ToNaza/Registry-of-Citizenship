31a97aa04d62a7dbcac2a3d351b015dc0c9cff53
ToNaza <misterket37@gmail.com>
2024-03-26 19:55:43 +0200

    test 0.6.2

--- RegisterJSua.js
+++ RegisterJSua.js
@@ -1,9 +1,1 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwLprY1KqntzO4rWmRuvbWm_MYUwppJGEaB_esZ9Xq3UwmGV9A12vo5bqgN6QU2evDv/exec'
-        const form = document.forms['submit-to-google-sheet']
-      
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => console.log('Success!', response))
-            .catch(error => console.error('Error!', error.message))
-        })
+     
